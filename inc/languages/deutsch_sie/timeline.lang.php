df28028ac33bfce3871d231c8c79e320e2ae2c2d
Lara <75126217+little-evil-genius@users.noreply.github.com>
2021-07-17 15:28:42 +0200

    Create timeline.lang.php

---
+++ inc/languages/deutsch_sie/timeline.lang.php
@@ -0,0 +1,53 @@
+<?php
+$l['timeline'] = "Zeitstrahl";
+
+// HINZUFÜGEN UND BEARBEITEN
+$l['timeline_add'] = "Ereignis hinzufügen";
+$l['timeline_add_name'] = "Titel vom Ereignis";
+$l['timeline_add_type'] = "Timeline vom Ereignis";
+$l['timeline_add_name_desc'] = "Kurzer Titel vom Ereignis";
+$l['timeline_add_desc'] = "Beschreibung vom Ereignis";
+$l['timeline_add_desc_desc'] = "Beschreibe hier das Ereignis ausführlich. Dir stehen 5000 Zeichen dafür frei.";
+$l['timeline_add_date'] = "Datum vom Ereignis";
+$l['timeline_edit'] = "Ereignis bearbeiten";
+
+
+// MOD-CP
+$l['timeline_modcp_nav'] = "Zeitstrahl-Ereignisse";
+$l['timeline_modcp'] = "Zeitstrahl-Ereignisse freischalten";
+$l['timeline_modcp_accepted'] = "Zeitstrahl-Ereignisse annehmen";
+$l['timeline_modcp_delete'] = "Zeitstrahl-Ereignisse ablehnen";
+
+// PRIVATE NACHRICHTEN
+$l['timeline_modcp_pm_delete_subject'] = "Dein Zeitstrahl-Ereignisse wurde abgelehnt!";
+$l['timeline_modcp_pm_delete_message'] = "Du hast ein Ereignis für unseren Zeitstrahl erstellt gehabt.
+Doch musste ich diesen leider ablehnen. Solltest du Fragen haben, wieso ich ihn ablehnen musste, meld dich bei mir oder beim restlichen Team.";
+$l['timeline_modcp_pm_accept_subject'] = "Dein Zeitstrahl-Ereignisse wurde angenommen!";
+$l['timeline_modcp_pm_accept_message'] = "Du hast ein Ereignis für unseren Zeitstrahl erstellt.
+Ich habe diesen gerade überprüft und angenommen. Du findest dein Ereignis nun in unserem Zeistrahl.
+Danke, dass du unseren Zeitstrahl mit Informationen erweiterst.";
+
+
+// MONATE
+$l['timline_month_january'] = "Januar";
+$l['timline_month_february'] = "Februar";
+$l['timline_month_march'] = "März";
+$l['timline_month_april'] = "April";
+$l['timline_month_may'] = "Mai";
+$l['timline_month_june'] = "Juni";
+$l['timline_month_july'] = "Juli";
+$l['timline_month_august'] = "August";
+$l['timline_month_september'] = "September";
+$l['timline_month_october'] = "Oktober";
+$l['timline_month_november'] = "November";
+$l['timline_month_december'] = "Dezember";
+
+// WEITERLEITUNGS-SEITEN
+$l['timeline_new_event'] = "Das neue Ereignis wurde erfolgreich eingetragen! Du wirst nun zurückgeleitet.";
+$l['timline_edit_event'] = "Das Ereignis wurde erfolgreich bearbeitet! Du wirst nun zurückgeleitet.";
+$l['timline_delete_event'] = "Das Ereignis wurde erfolgreich gelöscht! Du wirst nun zurückgeleitet.";
+$l['timline_modcp_delete_event'] = "Das Ereignis wurde erfolgreich abgelehnt und gelöscht! Du wirst nun zurückgeleitet.";
+$l['timline_modcp_accept_event'] = "Das Ereignis wurde erfolgreich angenommen! Du wirst nun zurückgeleitet.";
+
+
+?>
